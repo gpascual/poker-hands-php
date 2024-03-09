8f32107eff109186793a8f41956e0ca045a15da1
Gonzalo Pascual <gonzalopascual.gpc@gmail.com>
2024-03-09 13:21:27 +0100

    red: adds the three of a kind comparison tests

--- tests/Unit/HandComparisonTest.php
+++ tests/Unit/HandComparisonTest.php
@@ -1,6 +1,41 @@
 <?php
 
-describe('comparign hands', function () {
+describe('comparing hands', function () {
+    describe(
+        'given a hand with three of a kind',
+        function () {
+            it(
+                'should declare winner the hand with the highest three of a kind',
+                function ($expectedResult, $inputHands) {
+                    $result = $this->pokerHands->whoWins($inputHands);
+
+                    expect($result)->toBe($expectedResult);
+                }
+            )->with([
+                [
+                    'White wins. - with three of a kind: 8',
+                    'Black: 2H 3D 5S 9C AD  White: 2C 8H 8S 8C KH',
+                ],
+                [
+                    'White wins. - with three of a kind: 8',
+                    'Black: 2H 3D 9S 9C AD  White: 2C 8H 8S 8C KH',
+                ],
+                [
+                    'Black wins. - with three of a kind: 5',
+                    'Black: 2H 5D 5S 5C KD  White: 2C 3H 4S 8C QH',
+                ],
+                [
+                    'Black wins. - with three of a kind: 9',
+                    'Black: 2H 3D 9S 9C 9D  White: 2C 8H 8S 8C KH',
+                ],
+                [
+                    'White wins. - with three of a kind: 6',
+                    'Black: 2H 3D 5S 5C 5D  White: 6D 6H 6C 9S KH',
+                ],
+            ]);
+        }
+    );
+
     describe(
         'given a hand with a pair',
         function () {
